453ce4c15eb9773e105a2548f445277c62ca3c1f
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-28 22:14:30 -0400

    added method for delete ticket employee assigned

--- src/models/EmployeesAssignedTickets/EmployeesAssignedTicketsModel.php
+++ src/models/EmployeesAssignedTickets/EmployeesAssignedTicketsModel.php
@@ -15,18 +15,28 @@ class EmployeesAssignedTicketsModel extends Model
      *  "id_ticket"=> 1
      * ]
      */
-    public function create($data){
+    public function create($data)
+    {
         $this->db()
             ->table('employees_assigned_tickets')
             ->insert($data);
     }
 
-    public function getByTicketId($ticketId){
+    public function getByTicketId($ticketId)
+    {
         return $this->db()
             ->select('employees.id_employee, employees.first_name, employees.last_name, employees.email,employees_assigned_tickets.create_at')
             ->table('employees_assigned_tickets')
-            ->innerJoin('employees','employees.id_employee','=','employees_assigned_tickets.id_employee')
-            ->where('id_ticket','=',$ticketId)
+            ->innerJoin('employees', 'employees.id_employee', '=', 'employees_assigned_tickets.id_employee')
+            ->where('id_ticket', '=', $ticketId)
             ->getAll();
+    }
+
+    public function remove($idTicket, $idEmployee)
+    {
+        $this->db()
+            ->table('employees_assigned_tickets')
+            ->where('id_ticket = ? AND id_employee = ?', [$idTicket, $idEmployee])
+            ->delete();
     }
 }
