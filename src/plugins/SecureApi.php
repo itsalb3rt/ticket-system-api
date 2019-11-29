82e6087088ffb65ab56c76365a81160a6f657ef5
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-29 17:01:29 -0400

    added docs to methods

--- src/plugins/SecureApi.php
+++ src/plugins/SecureApi.php
@@ -20,6 +20,15 @@ class SecureApi
     private $response;
     private $acceptOrigin = '*';
 
+    /**
+     * This class use for check is the user send the authorization token
+     * in headers, this not required in all areas of the app.
+     *
+     * Example: In Auth area is no required, login.
+     *
+     * SecureApi constructor.
+     * @param bool $publicArea
+     */
     public function __construct(bool $publicArea = false)
     {
         $this->publicArea = $publicArea;
@@ -33,6 +42,12 @@ class SecureApi
         }
     }
 
+    /**
+     * This mechanism works by sending an OPTIONS HTTP method with
+     * Access-Control-Request-Method and Access-Control-Request-Headers in
+     * the header to notify the server about the type of request it wants to send.
+     * The response it retrieves determine if the actual request is allowed to be sent or not.
+     */
     private function cors(): void
     {
         if ($this->request->server->get('REQUEST_METHOD') == 'OPTIONS') {
@@ -49,6 +64,10 @@ class SecureApi
         header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
     }
 
+    /**
+     * this only check if the token have assigne user, if no exists in database
+     * return 403 status code
+     */
     private function isTokenValid(): void
     {
         $employeeToken = str_replace('Bearer ', '', $this->request->headers->get('authorization'));
